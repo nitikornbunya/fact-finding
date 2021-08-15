03bbd2971a6fe35b301934c875789dba37685e37
Nitikorn Bunyakulcharoen <nitikorn.bunya@gmail.com>
2021-08-16 03:08:22 +0700

    add static

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'static',
   head: {
     titleTemplate: '%s - fact-finding',
     title: 'fact-finding',
@@ -66,8 +65,9 @@ export default {
     }
   },
 
+  target: 'static',
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    buildDir: '.nuxt'
   }
 }
