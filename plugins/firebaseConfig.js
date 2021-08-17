b385e27629148cc11e97e1d506b8729f3f7b5baa
Nitikorn Bunyakulcharoen <nitikorn.bunya@gmail.com>
2021-08-17 11:31:22 +0700

    deleted key

--- plugins/firebaseConfig.js
+++ plugins/firebaseConfig.js
@@ -4,14 +4,7 @@ import 'firebase/firestore'
 // Initialize Cloud Firestore through Firebase
 if (!firebase.apps.length) {
     const firebaseConfig = {
-        apiKey: "AIzaSyBG-kb-Uw93GoGrVMlH2G-ZAALbYJ7PQtU",
-        // authDomain: "fact-finder-app.firebaseapp.com",
-        // databaseURL: "fact-finder-app.firebaseio.com",
-        projectId: "fact-finder-app",
-        storageBucket: "fact-finder-app.appspot.com",
-        messagingSenderId: "671482455594",
-        appId: "1:671482455594:web:563dc794751a147167c553",
-        measurementId: "G-WN05ZMGNTN"
+
     }
     firebase.initializeApp(firebaseConfig)
 }
